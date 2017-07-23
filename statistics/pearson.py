84da82914d6391deebda83efacc2fdf158db43ce
Brayan2016Linux <bradrd2009jp@gmail.com>
2017-07-22 19:14:30 -0600

    Update version 1.2, july 22th, 2017

--- statistics/pearson.py
+++ statistics/pearson.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 #!python
 #pearson.py
-<<<<<<< HEAD
 #Created: 07/21/2017
 #Last Updated: 07/21/2017
-=======
->>>>>>> 495a74b7688f9f800789dd3256c1c09b3133fc97
 
 #/*******************************************************
 #* Copyright (C) 2016-2017 Brayan Rodriguez D. <bradrd2009jp@gmail.com>
@@ -18,11 +15,8 @@
 
 #********************************************************
 #*Conjunto de herramientas estadísticas para el análisis*
-<<<<<<< HEAD
 #*univariado, variables cuantitativas por medio de OOP  *
-=======
 #*univariado, variables cuantitativas                   *
->>>>>>> 495a74b7688f9f800789dd3256c1c09b3133fc97
 #*Desarrollado por Brayan Rodríguez                     *
 #*Versión 1.2                                           *
 #********************************************************
@@ -30,7 +24,6 @@
 #Importa la libreria utilizando el nombre:
 #if __name__ == '__main__':
 
-<<<<<<< HEAD
 #Para importar librerías en carpetas aledañas a nivel superior del .py
 #import sys
 #sys.path.append("./<directorio>")
@@ -115,50 +108,6 @@ def pearson_normal_XY(valuesX, valuesY):
     covNXY = covariance_XY(valuesX, valuesY)
     return covNXY / (desvStNormalX * desvStNormalY)
 
-
 #llamada interna
 if __name__ == '__main__':
     print("You should import this one in your main project")
-=======
-#Para importar la librería
-import statistics
-
-#Para trabajar con dos variables cuantitativas
-
-#Devuelve la covarianza entre dos listas de variables X y Y
-def covarianceXY(valuesX, valuesY):
-    arithMeanX = statistics.arithmeticMean(valuesX)
-    arithMeanY = statistics.arithmeticMean(valuesY)
-    sumValueXY = sumValuesXY(valuesX, valuesY)
-    return sumValueXY / float(len(valuesX)) - (arithMeanX * arithMeanY)
-
-#Devuelve el coeficiente de pearson sigma (n) de dos variables X y Y
-def pearsonSigmaXY(valuesX, valuesY):
-    desvStSigmaX = statistics.standardDesviationSigma(valuesX)
-    desvStSigmaY = statistics.standardDesviationSigma(valuesY)
-    covXY = covarianceXY(valuesX, valuesY)
-    return covXY / (desvStSigmaX * desvStSigmaY)
-
-#Devuelve el coeficiente de pearson sigma (n) de dos variables X y Y
-def pearsonNormalXY(valuesX, valuesY):
-    datosCovNXY = PearsonCoefficient()
-    datosDesvStdNormal = Statistics()
-    desvStNormalX = statistics.standardDesviationNormal(valuesX)
-    desvStNormalY = statistics.standardDesviationNormal(valuesY)
-    covNXY = covarianceXY(valuesX, valuesY)
-    return covNXY / (desvStNormalX * desvStNormalY)
-
-#Devuelve la suma del procto de variables X y Y
-def sumValuesXY(valuesX, valuesY):
-    sumaXY = 0.0
-    #for <iterador> in range(vi, vf, conteo)
-    #for <iterator> in range(longitud)
-    #for <iterator> in <lista>
-    for i in range(len(valuesX)):
-        sumaXY = sumaXY + valuesX[i - 1] * valuesY[i - 1]
-    return sumaXY
-
-#llamada interna
-if __name__ == '__main__':
-    print("You should import this one in your main project")
->>>>>>> 495a74b7688f9f800789dd3256c1c09b3133fc97
